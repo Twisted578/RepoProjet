5045fab0574790533a220e58c15e58f6507d15cd
Guénolé <guenole578@gmail.com>
2017-05-16 15:20:41 +0200

    Ajout de row sur chapitres.php pour que ce soit plus simple à séparer les derners articles et les derniers com

--- chapitres.php
+++ chapitres.php
@@ -6,6 +6,9 @@
        ?>
 
        <section class="lastArticles boxArticles">
+       <div class="lastchapitres">
+        <div class="row">
+            <div class="col-sm-6 col-xs-12">
 				<h3>Derniers articles </h3>
 				<ul>
 					<li>
@@ -59,8 +62,12 @@
 						</div>
 					</li>
 				</ul>
-			</section>
+		</section>
+		</div>
+			</div>
 			<section class="lastComments boxComments">
+			<div class="row">
+            <div class="col-sm-6 col-xs-12">
 				<h3>Derniers commentaires</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. </p>
@@ -73,19 +80,10 @@
 				tempor incididunt ut labore et dolore magna aliqua. </p>
 				<p>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat. Duis aute irure </p>
-			</section>
-			<div class="clearfix"></div>
-			<div class="accueil">
-				<h3>A propos</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
-			
-       
+			</div>
+			</section>
+		</div>
 
 
 
