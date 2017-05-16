16e2fca7d9424666fc749dde3b64f8a3412143f3
Guénolé <guenole578@gmail.com>
2017-05-16 19:06:25 +0200

    Ajout des colonnes v2

--- chapitres.php
+++ chapitres.php
@@ -1,95 +1,98 @@
-       <?php 
-       include 'INC/includes.php';
-       include 'INC/header.php';
+<?php 
 
-       ini_set('display_errors',1); 
-       ?>
+    include 'INC/includes.php';
+    include 'INC/header.php';
+    ini_set('display_errors',1); 
 
-       <section class="lastArticles boxArticles">
-       <div class="lastchapitres">
-        <div class="row">
-            <div class="col-sm-6 col-xs-12">
-				<h3>Derniers articles </h3>
-				<ul>
-					<li>
-						<a href="">
-							<div class="thumb"><img src="img/img1.png" alt ="" /></div>
-							<div class="detail">
-								<h4>Final FAntasy XIII</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt ut labore et dolore magna aliqua.  </p>
-							</div>
-						</a>
-					</li>
-					<li>
-						<div class="thumb"><img src="img/img2.png" alt ="" /></div>
-						<div class="detail">
-							<h4>Final FAntasy XIII</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua.  </p>
-						</div>
-					</li>
-					<li>
-						<div class="thumb"><img src="img/img3.png" alt ="" /></div>
-						<div class="detail">
-							<h4>Final FAntasy XIII</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua.  </p>
-						</div>
-					</li>
-					<li>
-						<div class="thumb"><img src="img/img4.png" alt ="" /></div>
-						<div class="detail">
-							<h4>Final FAntasy XIII</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua.  </p>
-						</div>
-					</li>
-					<li>
-						<div class="thumb"><img src="img/img5.png" alt ="" /></div>
-						<div class="detail">
-							<h4>Final FAntasy XIII</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua.  </p>
-						</div>
-					</li>
-					<li>
-						<div class="thumb"><img src="img/img6.png" alt ="" /></div>
-						<div class="detail">
-							<h4>Final FAntasy XIII</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua.  </p>
-						</div>
-					</li>
-				</ul>
-		</section>
+?>
+  		</br>
+  		</br>
+		<div class="lastchapitres">
+		    <div class="row">
+		    	<div class="col-lg-6">
+		    		 <section class="lastArticles boxArticles">
+						<h3>Derniers articles </h3>
+						<ul>
+							<li>
+								<a href="">
+									<div class="thumb"><img src="img/img1.png" alt ="" /></div>
+									<div class="detail">
+										<h4>Final FAntasy XIII</h4>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.  </p>
+									</div>
+								</a>
+							</li>
+							<li>
+								<div class="thumb"><img src="img/img2.png" alt ="" /></div>
+								<div class="detail">
+									<h4>Final FAntasy XIII</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.  </p>
+								</div>
+							</li>
+							<li>
+								<div class="thumb"><img src="img/img3.png" alt ="" /></div>
+								<div class="detail">
+									<h4>Final FAntasy XIII</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.  </p>
+								</div>
+							</li>
+							<li>
+								<div class="thumb"><img src="img/img4.png" alt ="" /></div>
+								<div class="detail">
+									<h4>Final FAntasy XIII</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.  </p>
+								</div>
+							</li>
+							<li>
+								<div class="thumb"><img src="img/img5.png" alt ="" /></div>
+								<div class="detail">
+									<h4>Final FAntasy XIII</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.  </p>
+								</div>
+							</li>
+							<li>
+								<div class="thumb"><img src="img/img6.png" alt ="" /></div>
+								<div class="detail">
+									<h4>Final FAntasy XIII</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.  </p>
+								</div>
+							</li>
+						</ul>
+				</section>		
+		    	</div>
+		    	<div class="col-lg-6">
+		    		<section class="lastComments boxComments">
+						<h3>Derniers commentaires</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. </p>
+						<p>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure </p>
+						<p>dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. </p>
+						<p>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure </p>
+					</section>
+		    	</div>
+		    	
+		    </div>
 		</div>
-			</div>
-			<section class="lastComments boxComments">
-			<div class="row">
-            <div class="col-sm-6 col-xs-12">
-				<h3>Derniers commentaires</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. </p>
-				<p>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure </p>
-				<p>dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. </p>
-				<p>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure </p>
-			</div>
-			</div>
-			</section>
-		</div>
 
 
 
-       <?php include 'INC/contact_moi.php'; ?>
+ 
+    <?php include 'INC/contact_moi.php'; ?>
             
-            <?php include 'INC/footer.php'; ?>
+    <?php include 'INC/footer.php'; ?>
                 
-        </body>
-    </html>
+    </body>
+</html>
+
